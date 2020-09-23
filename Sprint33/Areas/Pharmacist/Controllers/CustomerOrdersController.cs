25c5122c3e71ae9b52153076dccd93257a8369fa
cameronpeters87 <cameronpeters87@gmail.com>
2020-09-24 01:14:34 +0200

    Added Pharmacist overview

---
+++ Sprint33/Areas/Pharmacist/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sprint33.Areas.Pharmacist.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        // GET: Pharmacist/CustomerOrders
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Sprint33/Areas/Pharmacist/Controllers/DashboardController.cs
+++ Sprint33/Areas/Pharmacist/Controllers/DashboardController.cs
@@ -1,14 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using Sprint33.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 
 namespace Sprint33.Areas.Pharmacist.Controllers
 {
     public class DashboardController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Pharmacist/Dashboard
         public ActionResult Index()
         {
+            ViewBag.TotalPurchase = db.StockOrders.ToList().Count;
+            ViewBag.TotalOnlineSales = db.CustomerOrders.ToList().Count;
+            ViewBag.TotalInstoreSales = db.InStoreSales.ToList().Count;
+            ViewBag.TotalCustomers = db.Patients.ToList().Count;
+            ViewBag.TotalSuppliers = db.Suppliers.ToList().Count;
+            ViewBag.Products = db.Products.ToList().Count;
+
+            ViewBag.TotalExpense = db.StockOrders.Sum(s => (float?)s.TotalCost) ?? 0f;
+            ViewBag.TotalIncome = SumTotalInStoreSales(db) + SumTotalOnlineSales(db);
+            ViewBag.ProfitLoss = ViewBag.TotalIncome - ViewBag.TotalExpense;
+
+            ViewBag.LowStockMedicines = db.Products.Where(p => p.Quantity < 50)
+                .ToList();
+
             return View();
+        }
+
+        private float? SumTotalOnlineSales(ApplicationDbContext db)
+        {
+            return db.CustomerOrders.Sum(c => (float?)c.TotalCost) ?? 0f;
+        }
+        private float? SumTotalInStoreSales(ApplicationDbContext db)
+        {
+            return db.InStoreSales.Sum(c => (float?)c.TotalCost) ?? 0f;
         }
     }
 }
