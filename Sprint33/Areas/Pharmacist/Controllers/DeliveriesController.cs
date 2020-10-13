421b5989b5a3357c28447b88115a54fd7228b11b
cameronpeters87 <cameronpeters87@gmail.com>
2020-10-13 21:26:21 +0200

    Added Deliveries

---
+++ Sprint33/Areas/Pharmacist/Controllers/DeliveriesController.cs
@@ -0,0 +1,22 @@
+﻿using Sprint33.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Sprint33.Areas.Pharmacist.Controllers
+{
+    public class DeliveriesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Pharmacist/Deliveries
+        public ActionResult Index()
+        {
+            var orders = db.CustomerOrders.Where(o => o.OrderStatus.ProcessNumber == 4).OrderByDescending(o => o.Id).ToList();
+            return View(orders);
+        }
+
+        public ActionResult Directions()
+        {
+            return View();
+        }
+    }
+}
